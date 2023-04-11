349d187cb38003466072ce339a44118966cb6e33
Wilson Wong <wilson.shen97@gmail.com>
2023-04-11 21:02:56 +0800

    added state notifier

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,32 @@ class App extends StatelessWidget {
   }
 }
 
-// riverpod Consumer
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final current = this;
+
+    if (current != null) {
+      return current + (other ?? 0) as T;
+    }
+
+    if (other != null) {
+      return other + (current ?? 0) as T;
+    }
+
+    return null;
+  }
+}
+
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
+
+  void increment() => state = state == null ? 1 : state + 1;
+}
+
+final counterProvider = StateNotifierProvider<Counter, int?>(
+  (ref) => Counter(),
+);
+
 class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -31,7 +56,24 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Counter App'),
+      ),
+      body: Column(
+        children: [
+          Consumer(
+            builder: (context, ref, child) {
+              final count = ref.watch(counterProvider);
+              return Text(
+                (count == null ? '0' : count.toString()),
+                style: Theme.of(context).textTheme.titleMedium,
+              );
+            },
+          ),
+          TextButton(
+            onPressed: ref.read(counterProvider.notifier).increment,
+            child: const Text('Increase count'),
+          ),
+        ],
       ),
     );
   }
